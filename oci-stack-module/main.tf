f0a8ffbc204a2d7e041aa8402bff2da193d01ff3
Joe Harrison <53116754+sudo-kraken@users.noreply.github.com>
2023-11-10 11:36:40 +0000

    Update main.tf

--- oci-stack-module/main.tf
+++ oci-stack-module/main.tf
@@ -322,7 +322,7 @@ resource "oci_core_volume_attachment" "extra_volume_attachment" {
   attachment_type                     = "paravirtualized"
   instance_id                         = oci_core_instance.vm_instance_ampere.id
   volume_id                           = oci_core_volume.vm_instance_oci_stack_core_volume.id
-  device                              = "/dev/oracleoci/oraclevdb"
+  device                              = "/dev/oracleoci/oracleextravol"
   display_name                        = "oci_stack-core-volume-attachment"
   is_pv_encryption_in_transit_enabled = true
   is_read_only                        = false
