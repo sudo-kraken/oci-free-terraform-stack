6ef844a11cf19d07d415e9ef75247b7fab650536
Joe Harrison <53116754+sudo-kraken@users.noreply.github.com>
2023-11-10 14:00:04 +0000

    Update main.tf

--- oci-stack-module/main.tf
+++ oci-stack-module/main.tf
@@ -321,21 +321,3 @@ resource "oci_core_volume_backup_policy_assignment" "oci_stack_boot_volume_backu
     oci_core_instance.vm_instance_ampere
   ]
 }
-
-# Output the "list" of all availability domains.
-output "all-availability-domains-in-your-tenancy" {
-  value = data.oci_identity_availability_domains.ads.availability_domains
-}
-
-output "compartment-name" {
-  value = oci_identity_compartment.oci_stack.name
-}
-
-output "public-ip-x86_64-instances" {
-  value = oci_core_instance.vm_instance_x86_64.*.public_ip
-}
-
-output "public-ip-ampere-instance" {
-  value = oci_core_instance.vm_instance_ampere.public_ip
-}
-
