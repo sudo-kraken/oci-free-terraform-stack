223b7d3e0a0b80d8fc46b551b6479dbfa686ddac
Joe Harrison <53116754+sudo-kraken@users.noreply.github.com>
2023-11-12 09:57:26 +0000

    Update outputs.tf
    added more outputs and descriptions

--- oci-stack-module/outputs.tf
+++ oci-stack-module/outputs.tf
@@ -1,16 +1,29 @@
-# Output the "list" of all availability domains.
-output "all-availability-domains-in-your-tenancy" {
-  value = data.oci_identity_availability_domains.ads.availability_domains
-}
-
-output "compartment-name" {
-  value = oci_identity_compartment.oci_stack.name
-}
-
 output "public-ip-x86_64-instances" {
+  description = "The public IP addresses assigned to the E2 instances"
   value = oci_core_instance.vm_instance_x86_64.*.public_ip
 }
 
+output "private-ip-x86_64-instances" {
+  description = "The private IP addresses assigned to the E2 instances"
+  value = oci_core_instance.vm_instance_x86_64.*.private_ip
+}
+
+output "instance-id-x86_64-instances" {
+  description = "The OCID of the E2 instances"
+  value = oci_core_instance.vm_instance_x86_64.*.id
+}
+
 output "public-ip-ampere-instance" {
+  description = "The public IP addresse assigned to the A2 instance"
   value = oci_core_instance.vm_instance_ampere.public_ip
+}
+
+output "private-ip-ampere-instance" {
+  description = "The private IP addresse assigned to the A2 instance"
+  value = oci_core_instance.vm_instance_ampere.private_ip
+}
+
+output "instance-id-ip-ampere-instance" {
+  description = "The OCID of the A1 instance"
+  value = oci_core_instance.vm_instance_ampere.id
 }
